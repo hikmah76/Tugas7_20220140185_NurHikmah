fa21aec9db89dcd558b2d22cca3e5b062f28d6c1
nur hikmah <nur.hikmah190504@gmail.com>
2024-04-05 14:20:35 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Menambahkan event listener pada element video
+video.addEventListener("click", function video(){
+    // Menampilkan alert ketika video diklik
+    alert("ini video Red Velvet");
+  });
+  
+  // Menambahkan event listener pada element audio
+  audio.addEventListener("click", function audio(){
+    // Menampilkan alert ketika audio diklik
+    alert("ini Audio Red Velvet");
+  });
+  
+  // Menambahkan event listener pada element gambar
+  gambar.addEventListener("click", function gambar(){
+    // Menampilkan alert ketika gambar diklik
+    alert("ini gambar Red Velvet");
+  });
+  
